16316a71331c378cc98ad63765a0ed9c23c88f12
Sabbir Ahmed <sabbirs93@gmail.com>
2018-10-26 00:59:57 +0600

    Add files via upload

---
+++ uva/12700 - Banglawash.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+
+int main() {
+	char s[1024];
+	int n, d = 0;
+	scanf("%*d");
+	while(scanf("%d", &n) == 1) {
+		scanf("%s", s);
+		int c[128] = {};
+		for(int i = 0; i < n; i++)
+			c[s[i]]++;
+		printf("Case %d: ", ++d);
+		if(c['B'] > 0 && c['W'] == 0 && c['T'] == 0)
+			puts("BANGLAWASH");
+		else if(c['W'] > 0 && c['B'] == 0 && c['T'] == 0)
+			puts("WHITEWASH");
+		else if(c['A'] == n)
+			puts("ABANDONED");
+		else if(c['B'] > c['W'])
+			printf("BANGLADESH %d - %d\n", c['B'], c['W']);
+		else if(c['B'] < c['W'])
+			printf("WWW %d - %d\n", c['W'], c['B']);
+		else
+			printf("DRAW %d %d\n", c['W'], c['T']);
+	}
+	return 0;
+}
