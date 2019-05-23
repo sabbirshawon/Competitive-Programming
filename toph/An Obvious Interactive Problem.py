fe08974c6fd50bf75faf510e0968a9b913974d96
Sabbir Ahmed <sabbirs93@gmail.com>
2019-05-23 13:20:51 +0600

    Create 	An Obvious Interactive Problem.py

---
+++ toph/An Obvious Interactive Problem.py
@@ -0,0 +1,13 @@
+low = 0
+high = 1000000
+
+for i in range(1,26):
+    mid = (low + high) // 2
+    print(mid)
+    ans = input()
+    if ans == "Bigger":
+        low = mid + 1
+    elif ans == "Smaller":
+        high = mid - 1
+    else:
+        break
