88244454951b95c6451240c2e441cd33a2a3913a
Sabbir Ahmed <sabbirs93@gmail.com>
2019-05-23 13:17:55 +0600

    Create Thought Game.py

---
+++ toph/Thought Game.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(n):
+  a,b = list(map(int,input().split()))
+  add = a+b
+  avg = add//2
+  if(avg%2==0):
+    print("Sadia will be happy.")
+  else:
+    print("Oops!")
