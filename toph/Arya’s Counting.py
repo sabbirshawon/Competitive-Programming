69fc9a40527a84493a487e625a68ad4b6b5c7410
Sabbir Ahmed <sabbirs93@gmail.com>
2019-05-23 13:18:23 +0600

    Create Arya’s Counting.py

---
+++ toph/Arya’s Counting.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(n):
+  a,b,c = map(int,input().split())
+  if a>b and a>c:
+    print("Case {}: {}".format(i+1,"A"))
+  elif b>a and b>c:
+    print("Case {}: {}".format(i+1,"B"))
+  elif c>a and c>b:
+    print("Case {}: {}".format(i+1,"C"))
+  else:
+    print("Case {}: {}".format(i+1,"Confused"))
